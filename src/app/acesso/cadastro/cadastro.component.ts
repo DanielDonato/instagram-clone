72a8a151eb0fc2fa62ca000df67aac0acff5a32b
DanielDonato <daniel_donato10@yahoo.com.br>
2019-09-02 22:34:30 -0300

    Model usuario

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { Usuario } from '../usuario.model';
 
 @Component({
   selector: 'app-cadastro',
@@ -12,11 +15,30 @@ export class CadastroComponent implements OnInit {
   @Output()
   public exibirPainel: EventEmitter<string> = new EventEmitter<string>();
 
+  public formulario: FormGroup = new FormGroup({
+    'email' : new FormControl(null),
+    'nome_completo' : new FormControl(null),
+    'nome_usuario' : new FormControl(null),
+    'senha' : new FormControl(null)
+  });
+
   ngOnInit() {
   }
 
   public exibirPainelLogin(): void {
     this.exibirPainel.emit('login');
+  }
+
+  public cadastrarUsuario(): void {
+    console.log(this.formulario);
+    let usuario: Usuario = new Usuario(
+      this.formulario.value.email,
+      this.formulario.value.nome_completo, 
+      this.formulario.value.nome_usuario,
+      this.formulario.value.senha
+    );
+    console.log(usuario);
+    
   }
 
 }
